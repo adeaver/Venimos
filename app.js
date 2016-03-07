35d5ee186b86a961932cecb6adf7938c59ea3e0d
adeaver <deaver.andrew@gmail.com>
2016-03-07 14:34:01 -0500

    Added end points for menu and store information

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var path = require('path');
-var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var passport = require('passport');
-var expressSession = require('express-session');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use( bodyParser.json() ); 
@@ -12,16 +9,13 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressSession({secret: "notReallyASecret",
-	resave:false,
-	saveUninitialized:false}));
-app.use(passport.initialize());
-app.use(passport.session());
+
+var pizza = require('./routes/pizza.js');
 
 mongoose.connect('mongodb://pizza:thehutt@ds023478.mlab.com:23478/pizza4all');
+
+app.get('/store/:store_type/:address', pizza.getStores);
+app.get('/menu/:store_id', pizza.getStoreMenu);
 
 app.listen(3000);
---
+++ routes/pizza.js
@@ -0,0 +1,39 @@
+var pizzapi = require('dominos');
+var http = require('https');
+var bl = require('bl');
+
+var menuURL = ["https://order.dominos.com/power/store/", "/menu?lang=en&structured=true"]
+
+var pizzaRoutes = {};
+
+pizzaRoutes.getStores = function(req, res) {
+	var address = req.params.address;
+	var type = req.params.store_type;
+
+	pizzapi.Util.findNearbyStores(address, type, function (store) {
+		res.json(store);
+	});
+};
+
+pizzaRoutes.getStoreMenu = function(req, res) {
+	var sendURL = menuURL[0] + req.params.store_id + menuURL[1];
+
+	console.log(sendURL);
+
+	http.get(sendURL, function (response) {
+		response.pipe(bl(function(err, data) {
+			var sendData = JSON.parse(data.toString());
+			res.json(sendData);
+		}));
+	});
+};
+
+// pizzaRoutes.getOrderPrice = function(req, res) {
+// 	var order = new pizzapi.Order({
+// 		customer:req.params.customer_name,
+// 		storeID:req.params.store_id,
+// 		deliveryMethod:req.params.method
+// 	});
+// }
+
+module.exports = pizzaRoutes;
