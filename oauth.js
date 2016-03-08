5a15e3a0af0835985d24f01660346c20ef565d08
Casey Alvarado <alvarado.casey@gmail.com>
2016-03-08 15:50:57 -0500

    made authentication for login

---
+++ oauth.js
@@ -0,0 +1,11 @@
+
+var ids = {
+	// clientId: "ca_82cgoRl8OnBLBwBWinkKSHNK09sqbCFB" 
+	consumerKey: "wkWivWf42J5X8TApg40B7IizaHwEKXwcqu03tDfR", 
+	consumerSecret: "5O4P051emsjVnjVkC7axUTSieaL2s2lFRh4jaznC", 
+	requestTokenURL: "https://secure.splitwise.com/api/v3.0/get_request_token",
+	accessTokenURL: "https://secure.splitwise.com/api/v3.0/get_access_token",
+	authorizeURL: "https://secure.splitwise.com/authorize"
+};
+
+module.exports = ids;
