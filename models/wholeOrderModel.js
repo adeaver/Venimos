066997bdbd5694407d1f4f3f9f0d3e28a1f02f27
jagreene <austin.greene@students.olin.edu>
2016-05-04 20:27:53 -0400

    FEEDBACK

--- models/wholeOrderModel.js
+++ models/wholeOrderModel.js
@@ -1,14 +1,15 @@
 var mongoose = require('mongoose');
 
-var wholeOrderSchema = new mongoose.Schema({ 
+var wholeOrderSchema = new mongoose.Schema({
 	firstName: String,
 	lastName: String,
 	address: String,
 	email: String,
-	storeId: String, 
+	storeId: String,
 	splitwiseId:String,
-	friendsOrders: [{type:String}] 
-}); 
+	friendsOrders: [{type:String}]
+});
 
+// I might just do module.exports = mongoose.model, but that's very nitpicky
 var wholeOrder = mongoose.model('wholeOrders',  wholeOrderSchema);
-module.exports = wholeOrder; 
+module.exports = wholeOrder;
